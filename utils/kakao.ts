baeb178481a65c988ab61944daae753d12966757
namsugb <namsugb99@gmail.com>
2025-09-01 00:37:01 +0900

    alimtalk

--- utils/kakao.ts
+++ utils/kakao.ts
@@ -80,7 +80,7 @@ export async function sendKakaoNotificationToCustomer(data: KakaoNotificationDat
         // Lunasoft API 설정 확인
         const LUNA_USERID = process.env.LUNA_USERID;
         const LUNA_API_KEY = process.env.LUNA_API_KEY;
-        const LUNA_TEMPLATE_CODE = 50035;
+        const LUNA_TEMPLATE_CODE = 50037;
 
         if (!LUNA_USERID || !LUNA_API_KEY || !LUNA_TEMPLATE_CODE) {
             console.warn("Lunasoft 알림톡 설정이 되어있지 않습니다.");
@@ -88,6 +88,7 @@ export async function sendKakaoNotificationToCustomer(data: KakaoNotificationDat
         }
 
         const phoneNumber = data.phone.replace(/-/g, '');
+        const studio = "아침햇살 스튜디오";
 
         const requestBody = {
             userid: LUNA_USERID,
@@ -99,7 +100,7 @@ export async function sendKakaoNotificationToCustomer(data: KakaoNotificationDat
                     tel_num: phoneNumber,
                     use_sms: "0",
                     sms_content: "아침햇살 스튜디오 예약 문의",
-                    msg_content: `[아침햇살 스튜디오]\n${data.name}님 예약 문의 주셔서 감사합니다.]\n곧 상담 연락을 드리겠습니다.`,
+                    msg_content: `[${studio}]\n안녕하세요 ${data.name}님 예약 문의 주셔서 감사합니다.\n 곧 상담 연락을 드리겠습니다.궁금하신점이 있으시면 언제든 물어보세요`
                 },
             ],
         }
